1668ca7b9ddb2f9966b84cf4808a5e6dd4a20fb4
Ken Lu <ken@seeice.com>
2013-12-22 16:09:22 -0500

    test local credential for facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"]
+  #provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"]
+  #should read from env and yaml file, I will make it work for now.
+  provider :facebook, '1440319669516663', 'a5ba5679f8b7ee6d4d02e7d68c282c66'
 end
