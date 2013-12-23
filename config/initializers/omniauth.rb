729b8dd6e48fb5eb07498b2dfaf07095d9168a17
Ken Lu <ken@seeice.com>
2013-12-23 08:52:42 -0500

    checked again via created another key, which works under localhost:3000

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"]
   #should read from env and yaml file, I will make it work for now.
-  provider :facebook, '1440319669516663', 'a5ba5679f8b7ee6d4d02e7d68c282c66'
+  provider :facebook, '1445057605716906', 'a3e7f849c8e3371eee0f2a6ee62aef11'
 end
