06317d72684f38e9f87f99e30a959e058f3dfb2b
Matt Newton <newton10471@gmail.com>
2013-12-31 03:31:00 +0000

    require auth for create new blood drive

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"]
   #should read from env and yaml file, I will make it work for now.
-  ENV['facebook_key'], ENV['facebook_secret'] ='236566603181302', 'a9a5a24aee5527234bf5335c00497cd0'
   provider :facebook, ENV['facebook_key'], ENV['facebook_secret'] 
 end
