f15414e1111c0156d13ac2f6019a456cd193ff05
Matt Newton <newton10471@gmail.com>
2013-12-31 03:02:54 +0000

    use environment variables for facebook app id and secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"]
   #should read from env and yaml file, I will make it work for now.
-  provider :facebook, '1445057605716906', 'a3e7f849c8e3371eee0f2a6ee62aef11'
+  ENV['facebook_key'], ENV['facebook_secret'] ='236566603181302', 'a9a5a24aee5527234bf5335c00497cd0'
+  provider :facebook, ENV['facebook_key'], ENV['facebook_secret'] 
 end
