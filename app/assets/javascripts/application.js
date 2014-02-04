8f97f27b1738cbfbe23fcda511356ce274279434
Matt Newton <newton10471@gmail.com>
2014-02-04 03:10:43 +0000

    added select2 autocomplete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require twitter/bootstrap
+//= require select2
 //= require_tree .
---
+++ app/assets/javascripts/blood_drives.js
@@ -0,0 +1,5 @@
+$(function(){
+  $('#add-donor-to-drive #donor_id').select2({
+    width: 'resolve'
+    });
+});
