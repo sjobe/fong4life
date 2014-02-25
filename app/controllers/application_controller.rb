6b00dff68b1b5e2d0e713f0ae724e6464d58248e
Matt Newton <newton10471@gmail.com>
2014-02-25 03:17:30 +0000

    Move authentication to application layer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :check_login
+
   
   private
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  helper_method :current_user
+  
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    
+    def check_login
+      unless current_user
+        flash[:notice] = 'Please login first' 
+        redirect_to action: :index
+      end
+    end
+    
+    helper_method :current_user
 end
