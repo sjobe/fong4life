c0fad2728adb9acfdbf8460ec0ed2a839e261bca
Matt Newton <newton10471@gmail.com>
2014-04-16 02:38:48 +0000

    add high level method names for emergency donor selection

--- app/models/emergency.rb
+++ app/models/emergency.rb
@@ -10,4 +10,28 @@ class Emergency
   field :donor_found, type: Boolean, default: false
   field :donor_details, type: String
   field :blood_type, type: String
+  field :pending_matches, type: Array
+  field :contacted_matches, type: Array
+
+  after_create :find_and_contact_matches
+  
+  def find_and_contact_matches
+    # call populate matches
+    # call contact_matches
+  end
+  
+  def populate_matches
+    # store the matches in pending matches    
+  end
+  
+  def contact_matches
+    # Send SMS to the next x top matches in pending_matches
+    # It's going to move the contacted matches from pending_matches to contacted_matches
+  end
+  
+  def close_emergency(donor_found, donor_details)
+    # set donor found
+    # set donor_details if any
+  end
+  
 end
