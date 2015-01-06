e2f0da04d08331432b3877a18c1f985dd304d159
Matt Newton <newton10471@gmail.com>
2015-01-06 02:29:16 +0000

    Added a 'contact next batch' button and functionality

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -69,6 +69,6 @@ class EmergenciesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def emergency_params
-      params.require(:emergency).permit(:title, :description, :sms_message_text, :blood_group, :status)
+      params.require(:emergency).permit(:title, :description, :sms_message_text, :blood_group, :status, :contact_next_batch)
     end
 end
