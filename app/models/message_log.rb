f1168fc48c2bb5b065c12dd188369908234667b8
Matt Newton <newton10471@gmail.com>
2015-01-13 02:52:02 +0000

    Keep a message log of all emails/sms sent

---
+++ app/models/message_log.rb
@@ -0,0 +1,14 @@
+class MessageLog
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  has_one :donor
+  field :env, type: String
+  field :type, type: String
+  field :from, type: String
+  field :to, type: String
+  field :message, type: String
+  
+  TYPE_SMS = 'sms'
+  TYPE_EMAIL = 'email'
+end
