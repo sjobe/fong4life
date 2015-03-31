283393308145561aca9bdb51a9f4cda47c0c06dc
Matt Newton <newton10471@gmail.com>
2015-03-31 01:45:21 +0000

    enable omni auth login for Google

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"]
-  #should read from env and yaml file, I will make it work for now.
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'] 
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"]
 end
