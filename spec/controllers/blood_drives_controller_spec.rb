fad363ed590b572dee60b13c4aef08f18ade0a84
Matt Newton <newton10471@gmail.com>
2015-04-14 01:55:46 +0000

    Trying to get blood drive controller specs to pass

--- spec/controllers/blood_drives_controller_spec.rb
+++ spec/controllers/blood_drives_controller_spec.rb
@@ -23,7 +23,7 @@ describe BloodDrivesController do
   # This should return the minimal set of attributes required to create a valid
   # BloodDrive. As you add validations to BloodDrive, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "location" => "MyString" } }
+  let(:valid_attributes) { { 'location' => 'Serrekunda', 'date' => 3.weeks.from_now, 'description' => '2nd blood drive of the year' } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
